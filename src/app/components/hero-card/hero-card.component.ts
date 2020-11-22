30bbc837bb3dfbbf8dbbcd2218576684ef0ab4e9
AlondraCordoba <alondracordoba0720@gmail.com>
2020-11-22 15:22:00 -0600

    Ultimas modificaciones (console.log => etapas)

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, DoCheck} from '@angular/core';
 import { Router } from '@angular/router';
 
 // Se importa de la misma manera la propiedad Input, ya que este prepara al componente para prepararse (renderizarse), el componente padre lo manda llamar mediante el selector.
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-hero-card',
   templateUrl: './hero-card.component.html',
   styleUrls: ['./hero-card.component.css']
 })
-export class HeroCardComponent implements OnInit {
+export class HeroCardComponent implements OnInit, OnChanges, DoCheck{
 
   // Hero: any = [];
   // Recibir la infromacion de un componente padre.
@@ -19,14 +20,26 @@ export class HeroCardComponent implements OnInit {
   // Mandar informacion del hijo a un padre se necesita Output y este EventEmitter
   @Output() selectorHero: EventEmitter<number>;
 
+  // tslint:disable-next-line: variable-name
   constructor( private _aRouter: Router) {
     // console.log(this.PadreHero);
     this.selectorHero = new EventEmitter();
+    console.log('Constructor de hero-card');
    }
 
   ngOnInit(): void {
+    console.log('Evento ngOnInit de hero-card');
   }
 
+  // tslint:disable-next-line: typedef
+  ngOnChanges(){
+    console.log('Evento ngOnChanges de hero-card');
+  }
+    // tslint:disable-next-line: typedef
+  ngDoCheck(){
+    console.log('Evento ngDoCheck de hero-card');
+  }
+  // tslint:disable-next-line: typedef
   Navegar(){
     // Se pasara el index con el input y se agregara la propiedad index con un tipo number.
     // Esto vendra desde el padre (HerosComponent)
--- src/app/components/heros/heros.component.ts
+++ src/app/components/heros/heros.component.ts
@@ -1,29 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy} from '@angular/core';
 import { HeroService } from 'src/app/services/Hero.services';
 import { Router } from '@angular/router';
 
 // Decorador:
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
-export class HerosComponent implements OnInit {
+export class HerosComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 
-  constructor( private heroService: HeroService, private _aRouter: Router) { }
-
+  // tslint:disable-next-line: variable-name
+  constructor( private heroService: HeroService, private _aRouter: Router) {
+    console.log('Constructor de la clase(heros)');
+  }
+  Heros: any[] = [];
   ArrayHeros: any = [];
 
   // Se ejecuta cuando se termina de renderizar el componente.
   ngOnInit(): void {
     this.ArrayHeros = this.heroService.getHeros();
+    console.log('Evento ngOnInit (ubicado en hero)');
   }
+      // tslint:disable-next-line: typedef
+    ngDoCheck(){
+      console.log('Evento ngDoCheck (ubicado en hero)');
+    }
+    // tslint:disable-next-line: typedef
+    ngAfterContentInit(){
+      console.log('Evento AfterContentInit (ubicado en hero)');
+    }
+
+    // tslint:disable-next-line: typedef
+    ngAfterContentChecked(){
+      console.log('Evento AfterContentChecked (ubicado en hero)');
+    }
+
+     // tslint:disable-next-line: typedef
+     ngAfterViewInit(){
+      console.log('Evento AfterViewInit (ubicado en hero)');
+    }
+
+    // tslint:disable-next-line: typedef
+    ngAfterViewChecked(){
+      console.log('Evento AfterViewChecked (ubicado en hero)');
+    }
+
+     // tslint:disable-next-line: typedef
+     ngOnDestroy(){
+      console.log('Evento ngOnDestroy (ubicado en hero)');
+    }
 
   // El componente padre ya tiene la informacion que el componente hijo necesita, sin necesidad de ejecutar otro servicio en el hijo.
   // Importado desde angular Router
+  // tslint:disable-next-line: typedef
   Navegar(index){
     console.log(index);
-    this._aRouter.navigate(['../hero',index])
+    this._aRouter.navigate(['../hero', index]);
   }
+
+
 
 }
